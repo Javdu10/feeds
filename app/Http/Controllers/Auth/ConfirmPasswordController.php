e9eb78fd38d848ee537ae066c139badabe99c0b0
Javdu10 <jav.valentin@gmail.com>
2019-12-27 18:02:21 +0100

    add @codeCoverageIgnore to unused Class or Auth pckg

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ConfirmsPasswords;
 
+/**
+ * @codeCoverageIgnore
+ */
 class ConfirmPasswordController extends Controller
 {
     /*
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+/**
+ * @codeCoverageIgnore
+ */
 class LoginController extends Controller
 {
     /*
